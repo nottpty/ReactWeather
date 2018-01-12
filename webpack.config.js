305156a78de7cb36718517a641a87d8805210e5e
Patinya Yongyai <patinya.y@ku.th>
2018-01-12 14:25:05 +0700

    Add foundation and jQeury

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,20 @@
+var webpack = require('webpack');
+
 module.exports = {
-    entry: './app/app.jsx',
+    entry: [
+        'script!jquery/dist/jquery.min.js',
+        'script!foundation-sites/dist/foundation.min.js',
+        './app/app.jsx'
+    ],
+    externals: {
+        jquery: 'jQuery'
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            '$': 'jquery',
+            'jQuery': 'jquery'
+        })
+    ],
     output: {
         path: __dirname,
         filename: './public/bundle.js'
@@ -19,16 +34,14 @@ module.exports = {
         extensions: ['', '.js', '.jsx']
     },
     module: {
-        loaders: [
-            {
-                loader: 'babel-loader',
-                query: {
-                    presets: ['react', 'es2015']
-                },
-                test: /\.jsx?$/,
-                exclude: /(node_modules|bower_components)/
-            }
-        ]
+        loaders: [{
+            loader: 'babel-loader',
+            query: {
+                presets: ['react', 'es2015']
+            },
+            test: /\.jsx?$/,
+            exclude: /(node_modules|bower_components)/
+        }]
     },
     devtool: 'cheap-module-eval-source-map'
 };
